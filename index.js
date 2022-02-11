f849e5cea3290b334df1684968f604adcfe7b002
Beth <bethm.meeker@gmail.com>
2022-02-11 15:29:14 -0700

    remove import

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
-const http = require('http')
 
 const app = express()
 
